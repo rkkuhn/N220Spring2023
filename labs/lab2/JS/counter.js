2c45a4f999bbcbb21fc60777547148a3ba9aa827
rkkuhn <80010161+rkkuhn@users.noreply.github.com>
2023-07-05 15:16:11 -0400

    tinkering

---
+++ labs/lab2/JS/counter.js
@@ -0,0 +1,17 @@
+// Author: Kelly Kuhn
+// Class: N220
+// Date: 7-5-2023
+
+
+// Create an application that outputs to the console every
+// draw call to P5. This number should start a zero and
+// increase bye one every frame.
+
+// Global variable
+let i = 0;
+
+// Creating the size of the canvas
+function setup() {
+  createCanvas(400, 400);
+
+}
