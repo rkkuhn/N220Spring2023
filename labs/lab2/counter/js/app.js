89b13e4abbac1a20dc0a1f4a7702146d4d8b811b
rkkuhn <80010161+rkkuhn@users.noreply.github.com>
2023-07-05 20:10:39 -0400

    Trying to do Sub-Directories
    Trying to create sub-directories again and getting them working.

---
+++ labs/lab2/counter/js/app.js
@@ -0,0 +1,23 @@
+// Author: Kelly Kuhn
+// Class: N220
+// Date: 7-5-2023
+
+
+// Create an application that outputs to the console every
+// draw call to P5. This number should start a zero and
+// increase bye one every frame.
+
+// Global variable
+let i = 0;
+
+// Creating the size of the canvas
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  // This is the function that starts at zero and continues to
+  // count in an infinite loop. 
+      console.log(i++)
+  }
