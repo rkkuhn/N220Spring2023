52bc7c2235dbc5d85d84334ff9264fb0221f8c83
rkkuhn <80010161+rkkuhn@users.noreply.github.com>
2023-07-05 21:06:13 -0400

    Final Push
    Figured a way to get the counting function to advance on a single line instead of row by row. I preferred this layout. It allowed me to get away from using global variables.

--- labs/lab2/counter/js/app.js
+++ labs/lab2/counter/js/app.js
@@ -7,6 +7,9 @@
 // draw call to P5. This number should start a zero and
 // increase bye one every frame.
 
+// No canvas size required. After playing around I found that it counts 
+// on a single line instead of line by line. This layout I liked better.
+
 function draw() {
   // This is the function that starts at zero and continues to
   // count in an infinite loop.
