d9db437217997d26513a1e3cabe61c40c816cd44
rkkuhn <80010161+rkkuhn@users.noreply.github.com>
2023-07-10 20:07:06 -0400

    Push 3 out of the 5 completed
    Three out of the five have been completed. I will need to go in and clean up to make it more elegant.

---
+++ labs/lab4/clicktodrop/js/app.js
@@ -0,0 +1,47 @@
+
+    // Array to store bricks
+    let bricks = [];
+
+    function setup() {
+      createCanvas(400, 400);
+    }
+
+    function draw() {
+      background(0);
+
+      // Draw all the bricks
+      for (let i = 0; i < bricks.length; i++) {
+        bricks[i].update();
+        bricks[i].show();
+      }
+    }
+
+    function mouseClicked() {
+      // Create a new brick at the mouse position
+      let brick = new Brick(mouseX, mouseY);
+      bricks.push(brick);
+    }
+
+    // Brick class
+    class Brick {
+      constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.speed = 5;
+      }
+
+      // Update the brick's position
+      update() {
+        this.y += this.speed;
+      }
+
+      // Display the brick
+      show() {
+        fill(255, 0, 0);
+        rect(this.x, this.y, 50, 25);
+      }
+    }
+    
+    // Create a P5.js sketch
+    new p5();
+
