109078919c5e02236aaefeac128114a29fa68324
rkkuhn <80010161+rkkuhn@users.noreply.github.com>
2023-07-28 16:43:34 -0400

    Final push for stop light
    I finally finished the stop light assignment.

--- labs/lab8/stoplight/js/app.js
+++ labs/lab8/stoplight/js/app.js
@@ -1,31 +1,36 @@
 // Author: Kelly Kuhn
 // Date: July 27, 2023
 
-/* Create an application in P5 that changes a circle to 
-one of four different colors in an array when its 
-clicked. */
+/* Using an array for the three colors, draw a stoplight 
+    using a for loop and only one circle() call in your 
+    code. */
 
 
-
-
+// Define the colors for the stoplight
 // Global Variables
-var light = pickRandom([
-  'stop',
-  'slow',
-  'go',
-]);
+let colors = ["red", "yellow", "green"];
 
-// Print statement for the light
-print(light);
-if (light === 'stop'){
-  dropbox('red');
-
-}
-if (light === 'slow'){
-  dropbox('green');
+// Setup function to create the canvas size
+function setup(){
+  // Set the canvas size to fit the stoplight
+  createCanvas(700, 700);
 }
 
-if (light === 'go'){
-  dropbox('yellow');
-}
+// Start the draw function
+function draw(){
+  // Background color is gray-ish
+  background(220);
+  // Create the size of each circle
+  let circleSize = 80;
+  // Vertical spacing between circles
+  let spacing = 175;
 
+  // Start the for loop to draw the circles for each color using < 3 for the
+  // three colors - red yellow and green
+  for (let i = 0; i < 3; i++) {
+    // Draw the circles at different vertical positions 
+    // with different colors
+    fill(colors[i]);
+    circle(width / 2, spacing * (i + 1), circleSize);
+  }
+}
